932b325da63d788054c82e182d1ef0f03e163889
José Armando Gutiérrez <gutierrezjarr@gmail.com>
2018-03-30 16:13:50 -0800

    Fonfirmacion de funciones
    Se confirma que la funciones, trabajen adecuadamente

--- practica03.c
+++ practica03.c
@@ -2,21 +2,29 @@
 #include "tablas.h"
 #include "funciones.h"
 
+/*
+@param *c arreglo en el que se almacenará la permutación
+@param *K es la llave que se permutara
+@param pc1 es la tabla de permitación
+*/
 void setC(unsigned char *C, unsigned char *K, PC_1 pc1);
 
 int main()
 {
     PC_1 pc1;
+    IP ip1;
+    ip1 = newIP();
     pc1 = newPC1();
     int i=0, j=0, k=0;
     unsigned char M[8]={0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
     unsigned char K[8]={0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1};
     unsigned char C0[7]={0};
-    setC(C0, K, pc1);
-    printC(C0);
-    rolN(C0,5);
-    printf("\n");
-    printC(C0);
+    printC(M);
+    // setC(C0, K, pc1);
+    // printC(C0);
+    // rolN(C0,5);
+    // printf("\n");
+    // printC(C0);
     return 0;
 }
 
--- tablas.h
+++ tablas.h
@@ -4,7 +4,7 @@ typedef struct {
     unsigned char columnas;
 }PC_1;
 
-PC_1 newIP(void);
+PC_1 newPC1(void);
 
 typedef struct {
     unsigned char val[8][8];
@@ -12,7 +12,7 @@ typedef struct {
     unsigned char columnas;
 }IP;
 
-IP newPC1(void);
+IP newIP(void);
 
 typedef struct {
     unsigned char val[6][8];
@@ -21,11 +21,3 @@ typedef struct {
 }PC_2;
 
 PC_2 newPC2(void);
-// const unsigned int PC_2[6][8] = {
-//     {14, 17, 11, 24, 1, 5, 3, 28},
-//     {15, 6, 21, 10, 23, 19, 12, 4},
-//     {26, 8, 16, 7, 27, 20, 13, 2},
-//     {41, 52, 31, 37, 47, 55, 30, 40},
-//     {51, 45, 33, 48, 44, 49, 39, 56},
-//     {34, 53, 46, 42, 50, 36, 29, 32}
-// };
